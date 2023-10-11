578787f1ccbb24bc381a5f2940ac0ca918fd6421
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-11 17:26:37 -0300

    Inicio Aula 42

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,19 @@
+package com.luv2code.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+    
+    // define um campo privado para a dependencia
+    private Coach myCoach;
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
