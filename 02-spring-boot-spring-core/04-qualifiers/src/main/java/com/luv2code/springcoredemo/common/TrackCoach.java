d2750909256e5fe1d389d29a921f483baeb07f7a
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-11 17:41:15 -0300

    Final Aula 45

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springcoredemo.common;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Runa a hard 5k!";
+    }
+    
+}
