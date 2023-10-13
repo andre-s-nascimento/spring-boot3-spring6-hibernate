1980e2a1879914929153c3e1e5d0409c36d515b7
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-13 15:35:19 -0300

    Inicio Aula 55

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,7 +1,5 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
