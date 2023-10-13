c1d1deac21c3d3e044c0e49521a4726f2edab683
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-10-13 15:59:21 -0300

    Final Aula 59 - Config Bean

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
   private Coach myCoach;
 
   @Autowired
-  public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+  public DemoController(@Qualifier("aquatic") Coach theCoach) {
       System.out.println("In Constructor: " + getClass().getSimpleName());
     myCoach = theCoach;
   }
